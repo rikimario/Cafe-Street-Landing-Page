28ca7a34059579ebd51dad0567240eb3e84e782e
rikimario <mario_martinov@abv.bg>
2024-02-14 20:34:50 +0200

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Hero from "./components/Home";
+import Nav from "./components/Navigation";
 
 export default function App() {
   return (
     <>
+      <Nav />
       <Hero />
     </>
   );
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <>
+      <header className="fixed top-0 left-0 w-full z-50">
+        <a href="#"></a>
+        <ul>
+          <li>About us</li>
+          <li>Our Product</li>
+          <li>Delivery</li>
+        </ul>
+      </header>
+    </>
+  );
+}
