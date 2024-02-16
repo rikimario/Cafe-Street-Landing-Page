f1cfc09f1335480812bf6828f4c2a859107b35c4
rikimario <mario_martinov@abv.bg>
2024-02-16 23:14:50 +0200

    add popular section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Home";
 import Nav from "./components/Navigation";
+import Popular from "./components/Popular";
 
 export default function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Popular />
     </>
   );
 }
---
+++ src/components/Popular.jsx
@@ -0,0 +1,7 @@
+export default function Popular() {
+  return (
+    <section>
+      <h1>Popular</h1>
+    </section>
+  );
+}
