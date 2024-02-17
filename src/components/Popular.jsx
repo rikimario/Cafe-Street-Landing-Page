caf07ea3046633e7c6754c8490098eeaaca849ac
rikimario <mario_martinov@abv.bg>
2024-02-17 19:55:21 +0200

    refactor popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,17 @@
+import PopularRec from "../assets/imgs/popular-rec.svg";
+
 export default function Popular() {
   return (
-    <section>
-      <h1>Popular</h1>
+    <section className="relative px-20">
+      <div className="h-[35rem]">
+        <img
+          className="absolute bottom-80 right-[12.34rem]"
+          src={PopularRec}
+          alt=""
+        />
+
+        <div className="absolute top-[-14rem] left-52 flex"></div>
+      </div>
     </section>
   );
 }
