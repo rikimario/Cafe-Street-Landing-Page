92a016ba8f094d008143f0b0b52e97c6a8496268
rikimario <mario_martinov@abv.bg>
2024-02-29 23:44:47 +0200

    add special menu cards

--- src/components/SpecialMenu.jsx
+++ src/components/SpecialMenu.jsx
@@ -1,1 +1,158 @@
-export default function SpecialMenu() {}
+import VanillaLatte from "../assets/imgs/vanilla-latte-popular.svg";
+import ShoppingCartPop from "../assets/imgs/popular-shopping-cart.svg";
+
+export default function SpecialMenu() {
+  return (
+    <section className="pt-36 px-32 grid grid-cols-3">
+      <div className="grid justify-center items-center pb-24">
+        <div className="relative w-[21.45rem] h-[23.313rem] bg-[#fff] p-4 rounded-xl">
+          <img className="w-full" src={VanillaLatte} alt="vanilla latte" />
+          <div className="flex justify-between items-center text-2xl font-semibold text-[#2F2105]">
+            <h2 className="pl-4 py-4">Vanilla Latte</h2>
+            <p className="font-bold pr-6">21 K</p>
+          </div>
+          <div className="flex justify-left items-center pt-2">
+            <div className="flex pl-5 gap-4 font-medium">
+              <button className="px-3 py-1 border-2 border-solid border-[#FF902B] rounded-lg text-[#FF902B]">
+                Hot
+              </button>
+              <button className="px-3 py-1 border-2 border-solid border-[#FFD28F] rounded-lg text-[#FFD28F]">
+                Cold
+              </button>
+            </div>
+            <button className="pl-20">
+              <a href="#">
+                <img src={ShoppingCartPop} alt="" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid justify-center items-center pb-24">
+        <div className="relative w-[21.45rem] h-[23.313rem] bg-[#fff] p-4 rounded-xl">
+          <img className="w-full" src={VanillaLatte} alt="vanilla latte" />
+          <div className="flex justify-between items-center text-2xl font-semibold text-[#2F2105]">
+            <h2 className="pl-4 py-4">Vanilla Latte</h2>
+            <p className="font-bold pr-6">21 K</p>
+          </div>
+          <div className="flex justify-left items-center pt-2">
+            <div className="flex pl-5 gap-4 font-medium">
+              <button className="px-3 py-1 border-2 border-solid border-[#FF902B] rounded-lg text-[#FF902B]">
+                Hot
+              </button>
+              <button className="px-3 py-1 border-2 border-solid border-[#FFD28F] rounded-lg text-[#FFD28F]">
+                Cold
+              </button>
+            </div>
+            <button className="pl-20">
+              <a href="#">
+                <img src={ShoppingCartPop} alt="" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid justify-center items-center pb-24">
+        <div className="relative w-[21.45rem] h-[23.313rem] bg-[#fff] p-4 rounded-xl">
+          <img className="w-full" src={VanillaLatte} alt="vanilla latte" />
+          <div className="flex justify-between items-center text-2xl font-semibold text-[#2F2105]">
+            <h2 className="pl-4 py-4">Vanilla Latte</h2>
+            <p className="font-bold pr-6">21 K</p>
+          </div>
+          <div className="flex justify-left items-center pt-2">
+            <div className="flex pl-5 gap-4 font-medium">
+              <button className="px-3 py-1 border-2 border-solid border-[#FF902B] rounded-lg text-[#FF902B]">
+                Hot
+              </button>
+              <button className="px-3 py-1 border-2 border-solid border-[#FFD28F] rounded-lg text-[#FFD28F]">
+                Cold
+              </button>
+            </div>
+            <button className="pl-20">
+              <a href="#">
+                <img src={ShoppingCartPop} alt="" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid justify-center items-center pb-24">
+        <div className="relative w-[21.45rem] h-[23.313rem] bg-[#fff] p-4 rounded-xl">
+          <img className="w-full" src={VanillaLatte} alt="vanilla latte" />
+          <div className="flex justify-between items-center text-2xl font-semibold text-[#2F2105]">
+            <h2 className="pl-4 py-4">Vanilla Latte</h2>
+            <p className="font-bold pr-6">21 K</p>
+          </div>
+          <div className="flex justify-left items-center pt-2">
+            <div className="flex pl-5 gap-4 font-medium">
+              <button className="px-3 py-1 border-2 border-solid border-[#FF902B] rounded-lg text-[#FF902B]">
+                Hot
+              </button>
+              <button className="px-3 py-1 border-2 border-solid border-[#FFD28F] rounded-lg text-[#FFD28F]">
+                Cold
+              </button>
+            </div>
+            <button className="pl-20">
+              <a href="#">
+                <img src={ShoppingCartPop} alt="" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid justify-center items-center pb-24">
+        <div className="relative w-[21.45rem] h-[23.313rem] bg-[#fff] p-4 rounded-xl">
+          <img className="w-full" src={VanillaLatte} alt="vanilla latte" />
+          <div className="flex justify-between items-center text-2xl font-semibold text-[#2F2105]">
+            <h2 className="pl-4 py-4">Vanilla Latte</h2>
+            <p className="font-bold pr-6">21 K</p>
+          </div>
+          <div className="flex justify-left items-center pt-2">
+            <div className="flex pl-5 gap-4 font-medium">
+              <button className="px-3 py-1 border-2 border-solid border-[#FF902B] rounded-lg text-[#FF902B]">
+                Hot
+              </button>
+              <button className="px-3 py-1 border-2 border-solid border-[#FFD28F] rounded-lg text-[#FFD28F]">
+                Cold
+              </button>
+            </div>
+            <button className="pl-20">
+              <a href="#">
+                <img src={ShoppingCartPop} alt="" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid justify-center items-center pb-24">
+        <div className="relative w-[21.45rem] h-[23.313rem] bg-[#fff] p-4 rounded-xl">
+          <img className="w-full" src={VanillaLatte} alt="vanilla latte" />
+          <div className="flex justify-between items-center text-2xl font-semibold text-[#2F2105]">
+            <h2 className="pl-4 py-4">Vanilla Latte</h2>
+            <p className="font-bold pr-6">21 K</p>
+          </div>
+          <div className="flex justify-left items-center pt-2">
+            <div className="flex pl-5 gap-4 font-medium">
+              <button className="px-3 py-1 border-2 border-solid border-[#FF902B] rounded-lg text-[#FF902B]">
+                Hot
+              </button>
+              <button className="px-3 py-1 border-2 border-solid border-[#FFD28F] rounded-lg text-[#FFD28F]">
+                Cold
+              </button>
+            </div>
+            <button className="pl-20">
+              <a href="#">
+                <img src={ShoppingCartPop} alt="" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
